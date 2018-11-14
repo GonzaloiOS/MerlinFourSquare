27401c4235704157cf02b07ff202b807ad2ae690
Gonzalo Linares N <glinares1408@gmail.com>
2018-11-14 01:37:17 -0500

    First Commit.

---
+++ MerlinFourSquare/MerlinFourSquare/Model/NetworkManager.swift
@@ -0,0 +1,10 @@
+//
+//  NetworkManager.swift
+//  MerlinFourSquare
+//
+//  Created by Gonzalinho on 11/14/18.
+//  Copyright © 2018 Gonzalinho. All rights reserved.
+//
+
+import Foundation
+
---
+++ MerlinFourSquare/MerlinFourSquare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MerlinFourSquare
+//
+//  Created by Gonzalinho on 11/14/18.
+//  Copyright © 2018 Gonzalinho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
