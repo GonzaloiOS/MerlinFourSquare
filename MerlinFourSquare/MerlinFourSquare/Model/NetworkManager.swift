dd765d30e7d3e01ec5ffe60874c7f8a323d6d4e5
Gonzalo Linares N <glinares1408@gmail.com>
2018-11-14 06:13:52 -0500

    testing network class

--- MerlinFourSquare/MerlinFourSquare/Model/NetworkManager.swift
+++ MerlinFourSquare/MerlinFourSquare/Model/NetworkManager.swift
@@ -8,3 +8,32 @@
 
 import Foundation
 
+class NetworkManager {
+    static func performStandartRequest() {
+        
+        guard
+            let url = URL(string: "https://api.foursquare.com/v2/venues/search")
+        else {
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard
+                let data = data
+            else {
+                print("Service Error")
+                DispatchQueue.main.async(execute: {
+                    print("Completion closure")
+                })
+                return
+            }
+            
+            guard let 
+            
+        }
+        
+        task.resume()
+    }
+}
